784545cb131de7c9ce32c994ea0b5fb552c37fd1
David Little <littledavid175@gmail.com>
2019-07-07 19:55:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,12 @@
 def game_hash
-  :home => {:team_name => "Brooklyn Nets",
+  {:home => {:team_name => "Brooklyn Nets",
   :colors => ['Black', 'White'],
   :players => ["Alan Anderson" => {:number => 0, :Shoe => 16, :Points => 22, :Rebounds => 12, :Assists => 12, :Steals => 3, :Blocks => 1, :Slam_Dunks => 1},
   "Reggie Evans" => {:number => 30, :Shoe => 14, :Points => 12, :Rebounds => 12, :Assists => 12, :Steals => 12, :Blocks => 12, :Slam_Dunks => 7},
   "Brook Lopez" => {:number => 11, :Shoe => 17, :Points => 17, :Rebounds => 19, :Assists => 10, :Steals => 3, :Blocks => 1, :Slam_Dunks => 15},
   "Mason Plumlee" => {:number => 1, :Shoe => 19, :Points => 26, :Rebounds => 11, :Assists => 6, :Steals => 3, :Blocks => 8, :Slam_Dunks => 5},
   "Jason Terry" => {:number =>31, :Shoe => 15, :Points => 19, :Rebounds => 2, :Assists => 2, :Steals => 4, :Blocks => 11, :Slam_Dunks => 1}
-    ]
+    ]}
   
   :away => {:team_name => "Charlotte Hornets",
   :colors => ['Turquoise', 'Purple'],

b6942c5ccbcd12c07fd02d6057816951d00e9924
David Little <littledavid175@gmail.com>
2019-07-07 19:51:02 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,23 @@
-# Write your code here!
+def game_hash
+  :home => {:team_name => "Brooklyn Nets",
+  :colors => ['Black', 'White'],
+  :players => ["Alan Anderson" => {:number => 0, :Shoe => 16, :Points => 22, :Rebounds => 12, :Assists => 12, :Steals => 3, :Blocks => 1, :Slam_Dunks => 1},
+  "Reggie Evans" => {:number => 30, :Shoe => 14, :Points => 12, :Rebounds => 12, :Assists => 12, :Steals => 12, :Blocks => 12, :Slam_Dunks => 7},
+  "Brook Lopez" => {:number => 11, :Shoe => 17, :Points => 17, :Rebounds => 19, :Assists => 10, :Steals => 3, :Blocks => 1, :Slam_Dunks => 15},
+  "Mason Plumlee" => {:number => 1, :Shoe => 19, :Points => 26, :Rebounds => 11, :Assists => 6, :Steals => 3, :Blocks => 8, :Slam_Dunks => 5},
+  "Jason Terry" => {:number =>31, :Shoe => 15, :Points => 19, :Rebounds => 2, :Assists => 2, :Steals => 4, :Blocks => 11, :Slam_Dunks => 1}
+    ]
+  
+  :away => {:team_name => "Charlotte Hornets",
+  :colors => ['Turquoise', 'Purple'],
+  :players => ["Jeff Adrien" => {:number => 4, :Shoe => 18, :Points => 10, :Rebounds => 1, :Assists => 1, :Steals => 2, :Blocks => 7, :Slam_Dunks => 2},
+  "Bismack Biyombo" => {:number => 0, :Shoe => 16, :Points => 12, :Rebounds => 4, :Assists => 7, :Steals => 22, :Blocks => 15, :Slam_Dunks => 10},
+  "DeSagna Diop" => {:number => 2, :Shoe => 14, :Points => 24, :Rebounds => 12, :Assists => 12, :Steals => 4, :Blocks => 5, :Slam_Dunks => 5},
+  "Ben Gordon" => {:number => 8, :Shoe => 15, :Points => 33, :Rebounds => 3, :Assists => 2, :Steals => 1, :Blocks => 1, :Slam_Dunks => 0}
+  "Kemba Walker" => {:number => 33, :Shoe => 15, :Points => 6, :Rebounds => 12, :Assists => 12, :Steals => 7, :Blocks => 5, :Slam_Dunks => 12}
+    ]}
+    }
+end
 
 
 
